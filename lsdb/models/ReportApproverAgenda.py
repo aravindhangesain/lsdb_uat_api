7fd0674b60aad2ff43bd85b37a4adc826b701ea1
nagipvel <nagi@pvel.com>
2025-07-16 13:54:58 +0530

    Report Approver Agenda Grid Changes

--- lsdb/models/ReportApproverAgenda.py
+++ lsdb/models/ReportApproverAgenda.py
@@ -4,7 +4,7 @@ class ReportApproverAgenda(models.Model):
     approver = models.ForeignKey('ReportApprover',on_delete=models.CASCADE,null=True,blank=True)
     report_result = models.ForeignKey('ReportResult',on_delete=models.CASCADE,null=True,blank=True)
     pichina = models.CharField(max_length=32,null=True,blank=True)
-    author = models.CharField(max_length=32,null=True,blank=True)
+    author = models.ForeignKey('auth.User',on_delete=models.CASCADE,null=True,blank=True)
     contractually_obligated_date = models.DateTimeField(null=True,blank=True)
     status_pan = models.CharField(max_length=32,null=True,blank=True)
     date_verified = models.DateTimeField(null=True,blank=True)
--- lsdb/serializers/ReportApproverAgendaSerializer.py
+++ lsdb/serializers/ReportApproverAgendaSerializer.py
@@ -3,17 +3,20 @@ from lsdb.models import *
 
 class ReportApproverAgendaSerializer(serializers.ModelSerializer):
     report_type = serializers.ReadOnlyField(source='report_result.report_type_definition.name')
+    approver_name = serializers.ReadOnlyField(source='approver.name')
     project_number = serializers.ReadOnlyField(source='report_result.work_order.project.number')
     customer_name = serializers.ReadOnlyField(source = 'report_result.work_order.project.customer.name')
     bom = serializers.ReadOnlyField(source='report_result.work_order.name')
     project_manager_name = serializers.ReadOnlyField(source = 'report_result.work_order.project.project_manager.username')
     ntp_date = serializers.ReadOnlyField(source='report_result.work_order.start_datetime')
+    author_name = serializers.ReadOnlyField(soruce = 'author.username')
 
     class Meta:
         model = ReportApproverAgenda
         fields  = [
             'id',
             'approver',
+            'approver_name',
             'report_type',
             'project_number',
             'project_manager_name',
@@ -23,6 +26,10 @@ class ReportApproverAgendaSerializer(serializers.ModelSerializer):
             'report_result',
             'pichina',
             'author',
+            'author_name',
             'status_pan',
-            'contractually_obligated_date'
+            'contractually_obligated_date',
+            'date_verified',
+            'date_approved',
+            'date_delivered'
         ]
