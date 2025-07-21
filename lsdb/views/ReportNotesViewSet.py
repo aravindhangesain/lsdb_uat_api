ab7a733e861cbc5ae22cbf99425db5ab81020ddd
rohith-913 <rohith@gesain.net>
2025-07-21 13:58:20 +0530

    ReportWriterAgenda Normal flag and note added

--- lsdb/views/ReportNotesViewSet.py
+++ lsdb/views/ReportNotesViewSet.py
@@ -59,6 +59,42 @@ class ReportNotesViewSet(viewsets.ModelViewSet):
         new_note.save()
         return Response({"status": "note added"}, status=status.HTTP_201_CREATED)
     
+    @transaction.atomic
+    @action(detail=False, methods=['post'])
+    def add_flag(self, request):
+        self.context = {'request': request}
+        params = request.data
+        # try:
+        #     existing_note = ReportNotes.objects.get(pk=pk)
+        # except ReportNotes.DoesNotExist:
+        #     return Response({"error": f"Note with id {pk} not found."}, status=status.HTTP_404_NOT_FOUND)
+        # if params.get('reviewer'):
+        #     reviewer = ReportReviewer.objects.get(id=params.get('reviewer'))
+        #     existing_note.reviewer = reviewer
+        #     existing_note.save()
+        note_type = NoteType.objects.get(id=params.get('type'))
+        report = ReportResult.objects.get(id=params.get('report'))
+        reviewer = ReportReviewer.objects.get(id=params.get('reviewer'))
+        new_note = ReportNotes.objects.create(
+            user=request.user,
+            subject=params.get('subject'),
+            comment=params.get('comment'),
+            type=note_type,
+            reviewer=reviewer,
+            report=report
+            # parent_note=existing_note
+        )
+        if note_type.id in [1, 3]:
+            for label_id in params.get('labels', []):
+                ReportNoteLabels.objects.create(reportnote=new_note, label_id=label_id)
+            for user_id in params.get('tagged_users', []):
+                ReportNoteTaggedPM.objects.create(reportnote=new_note, project_manager_id=user_id)
+        else:
+            new_note.labels.set(params.get('labels', []))
+            new_note.tagged_users.set(params.get('tagged_users', []))
+        new_note.save()
+        return Response({"status": "note or flag added"}, status=status.HTTP_201_CREATED)
+    
     @action(detail=True, methods=['get'])
     def get_children(self, request, pk=None):
         self.context = {'request': request}
