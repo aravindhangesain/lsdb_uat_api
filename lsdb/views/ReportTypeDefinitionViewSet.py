943f9deab91c2aa93aafc79cd90beda1030ec34a
Aravindhan <aravindhan@gesain.net>
2025-07-29 16:59:44 +0530

    disposition filter added

--- lsdb/views/ReportTypeDefinitionViewSet.py
+++ lsdb/views/ReportTypeDefinitionViewSet.py
@@ -4,12 +4,26 @@ from lsdb.serializers import ReportTypeDefinitionSerializer
 from rest_framework.response import Response
 from django.db import IntegrityError, transaction
 from rest_framework.decorators import action
+import django_filters
+from lsdb.models import ReportTypeDefinition
+from django_filters.rest_framework import DjangoFilterBackend
+
+
+class ReportTypeDefinitionFilter(django_filters.FilterSet):
+    disposition_id = django_filters.NumberFilter(field_name="disposition_id")
+
+    class Meta:
+        model = ReportTypeDefinition
+        fields = ['disposition_id']
+
 
 
 
 class ReportTypeDefinitionViewSet(viewsets.ModelViewSet):
     queryset = ReportTypeDefinition.objects.all()
     serializer_class = ReportTypeDefinitionSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ReportTypeDefinitionFilter
 
     # @transaction.atomic()
     # @action(detail=False, methods=['get', 'post'],)
