89dd9003813449fb50f1e1a32874a1b22bdfa9c1
Aravindhan <aravindhan@gesain.net>
2025-08-20 12:49:49 +0530

    asset_info function added

--- lsdb/views/AssetCalibrationViewSet.py
+++ lsdb/views/AssetCalibrationViewSet.py
@@ -2,7 +2,8 @@ from rest_framework import viewsets
 from lsdb.models import Asset, AssetCalibration
 from lsdb.serializers import AssetCalibrationSerializer
 from django.db import connection
-
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 class AssetCalibrationViewSet(viewsets.ModelViewSet):
     logging_methods = ['POST', 'PUT', 'PATCH', 'DELETE']
@@ -26,3 +27,21 @@ class AssetCalibrationViewSet(viewsets.ModelViewSet):
                     "INSERT INTO lsdb_asset_asset_types (asset_id, assettype_id) VALUES (%s, %s)",
                     [asset.id, asset_calibration.asset_type.id]
                 )
+
+    @action(detail=False, methods=['post','get'])
+    def asset_info(self, request):
+        asset_id = request.data.get('asset_id')
+        if not asset_id:
+            return Response({"error": "Asset ID is required"}, status=400)
+
+        try:
+            asset_calibration = AssetCalibration.objects.get(asset_id=asset_id)
+            serializer = self.get_serializer(asset_calibration)
+            data = serializer.data
+            return Response({
+                "last_calibrated_date": data["last_calibrated_date"],
+                "days_to_next_calibration": data["days_to_next_calibration"]
+            }, status=200)
+
+        except AssetCalibration.DoesNotExist:
+            return Response({"error": "Asset Calibration not found"}, status=404)
