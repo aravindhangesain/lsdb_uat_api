e77a360cf7a8aa096f07d1b914f686fff96c5ddf
nagipvel <nagi@pvel.com>
2025-08-22 18:20:02 +0530

    Response for the checklist get has been added

--- lsdb/serializers/ChecklistReportSerializer.py
+++ lsdb/serializers/ChecklistReportSerializer.py
@@ -1,7 +1,30 @@
 from rest_framework import serializers
 from lsdb.models import *
+from lsdb.serializers import *
+from collections import defaultdict
 
 class ChecklistReportSerializer(serializers.ModelSerializer):
+    checklist = serializers.SerializerMethodField()
+
+    def get_checklist(self, obj):
+        sections = defaultdict(list)
+        for cl in obj.checklist.all():
+            sections[cl.category].append({
+                "id": cl.id,
+                "description": cl.check_point
+            })
+        result = []
+        for category, checkpoints in sections.items():
+            result.append({
+                "category": category,
+                "check_point": checkpoints
+            })
+        return result
+
     class Meta:
         model = ChecklistReport
-        fields = '__all__'
+        fields = [
+            'id',
+            'report_name',
+            'checklist'
+        ]
