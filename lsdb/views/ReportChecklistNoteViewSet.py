8f3ab90901d775444c7597d9dde1d2432e90cc89
rohith-913 <rohith@gesain.net>
2025-08-25 16:30:44 +0530

    reportchecklist note get

--- lsdb/views/ReportChecklistNoteViewSet.py
+++ lsdb/views/ReportChecklistNoteViewSet.py
@@ -13,9 +13,11 @@ class ReportChecklistNoteViewSet(viewsets.ModelViewSet):
 
     @action(detail=False, methods=['get'], url_path='by-report')
     def by_checklistreport(self, request):
-        report_id = request.query_params.get('report_id')
-        if not report_id:
-            return Response({"error": "Missing report_id in query parameters"}, status=400)
-        notes = self.queryset.filter(report_id=report_id)
-        serializer = self.get_serializer(notes, many=True)
-        return Response(serializer.data)
+            report_id = request.query_params.get('report_result_id')
+            checklist_id = request.query_params.get('checklist_id')
+            checklist_report_id = request.query_params.get('checklist_report_id')
+            if not all([report_id, checklist_id, checklist_report_id]):
+                return Response({"error": "Missing some parameters"}, status=400)
+            notes = self.queryset.filter(report_id=report_id, checklist_id=checklist_id, checklist_report_id=checklist_report_id).order_by('-datetime')
+            serializer = self.get_serializer(notes, many=True)
+            return Response(serializer.data)
