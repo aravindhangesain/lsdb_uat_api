a4861ecb9277350aab0280f11938908bf059f05a
Aravindhan <aravindhan@gesain.net>
2025-08-25 13:57:09 +0530

    report checklist with notes

--- lsdb/models/ReportChecklistData.py
+++ lsdb/models/ReportChecklistData.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class ReportChecklistData(models.Model):
     report = models.ForeignKey('ReportResult', on_delete=models.CASCADE, null=True, blank=True)
-    # checklist = models.ForeignKey('CheckList', on_delete=models.CASCADE)
+    checklist = models.ForeignKey('CheckList', on_delete=models.CASCADE)
     checklist_report = models.ForeignKey('ChecklistReport', on_delete=models.CASCADE, null=True, blank=True )
     status = models.BooleanField(default=False)
+    checklist_note = models.ForeignKey('ReportChecklistNote', on_delete=models.CASCADE, null=True, blank=True)
--- lsdb/views/ReportChecklistDataViewSet.py
+++ lsdb/views/ReportChecklistDataViewSet.py
@@ -11,15 +11,19 @@ class ReportChecklistDataViewSet(viewsets.ModelViewSet):
     def add_reportchecklistdata(self, request):
         report_id = request.data.get('report_id')
         checklist_report_id = request.data.get('checklist_report_id')
-        checklist_data_ids = request.data.get('checklist_data', [])
-        status = request.data.get('status', False)
+        checklist_data = request.data.get('checklist_data', [])
+        status = request.data.get('status')
+
         
 
-        for checklist_data_id in checklist_data_ids:
-            
-            ReportChecklistData.objects.create(report_id=report_id,checklist_report_id=checklist_report_id,checklist_id=checklist_data_id,status=status)
-                
-        return Response({"message": "ReportChecklistData entries created successfully."})
+        for item in checklist_data:
+            ReportChecklistData.objects.create(
+                report_id=report_id,
+                checklist_report_id=checklist_report_id,
+                checklist_id=item["id"],
+                checklist_note_id=item["checklist_note_id"] if "checklist_note_id" in item else None,
+                status=status
+            )
             
         
     
