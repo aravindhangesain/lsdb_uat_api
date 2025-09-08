343e20101c00b8666a23a12e919a1a80b9ac662f
Aravindhan <aravindhan@gesain.net>
2025-09-08 15:29:25 +0530

    bug fixed

--- lsdb/views/SubAssetViewSet.py
+++ lsdb/views/SubAssetViewSet.py
@@ -5,4 +5,5 @@ from lsdb.models import *
 class SubAssetViewSet(viewsets.ModelViewSet):
     queryset = SubAsset.objects.all()
     serializer_class = SubAssetSerializer
+    lookup_field='sub_asset_name'
   
