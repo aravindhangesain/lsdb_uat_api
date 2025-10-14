0794884c2bf3da0f7e15653f4e40a3e24f9d271c
rohith-913 <rohith@gesain.net>
2025-10-14 12:11:26 +0530

    report team bug fixed

--- lsdb/serializers/ReportTeamSerializer.py
+++ lsdb/serializers/ReportTeamSerializer.py
@@ -3,10 +3,32 @@ from lsdb.models import *
 
 class ReportTeamSerializer(serializers.ModelSerializer):
     writer_name = serializers.ReadOnlyField(source='writer.username')
-    approver_name = serializers.ReadOnlyField(source='approver.username')
-    reviewer_name = serializers.ReadOnlyField(source='reviewer.username')
-    report_name = serializers.ReadOnlyField(source='report_type.name')
+    approver_name = serializers.SerializerMethodField()
+    reviewer_name = serializers.SerializerMethodField()
+    report_name= serializers.ReadOnlyField(source='report_type.name')
 
+    def get_reviewer_name(self, obj):
+        try:
+            if obj.reviewer and getattr(obj.reviewer, 'username', None):
+                return obj.reviewer.username
+        except Exception:
+            return None
+
+        if getattr(obj, 'reviewer_pm', False):
+            return 'PM'
+        return None
+    
+    def get_approver_name(self,obj):
+
+        try:
+            if obj.approver and getattr(obj.approver, 'username', None):
+                return obj.approver.username
+        except Exception:
+            return None
+
+        if getattr(obj, 'is_projmanager', False):
+            return 'PM'
+        return None
 
     class Meta:
         model = ReportTeam
@@ -16,13 +38,12 @@ class ReportTeamSerializer(serializers.ModelSerializer):
             'report_type',
             'report_name',
             'reviewer', 
+            'reviewer_name',
+            'reviewer_pm',
             'writer',
             'writer_name',
             'approver',
             'approver_name',
-            'reviewer_name',
             'is_projmanager',
-            'duration',
-            'reviewer_pm'
+            'duration'
         ]   
-        
