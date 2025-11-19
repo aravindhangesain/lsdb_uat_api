dca32e6d916365a7af6719ebd2e94587d5b91109
Aravindhan <aravindhan@gesain.net>
2025-11-19 17:24:31 +0530

    datetime added in cron status api

--- lsdb/views/CronStatusViewSet.py
+++ lsdb/views/CronStatusViewSet.py
@@ -12,21 +12,35 @@ class CronStatusViewSet(ViewSet):
     @action(detail=False, methods=['get'])
     def status(self, request):
         with connection.cursor() as cursor:
-            # SQL query to check the status
-            query = """
+            # 1. Get latest "Cron job started successfully"
+            query_started = """
                 SELECT flag
                 FROM cronstatus
                 WHERE description = 'Cron job started successfully'
                 ORDER BY datetime DESC
                 LIMIT 1;
             """
-            cursor.execute(query)
-            result = cursor.fetchone()
+            cursor.execute(query_started)
+            result_started = cursor.fetchone()
 
-        # Check the flag value in the result
-        if result and result[0]:  # If result exists and flag is true
-            return Response({'status': True})
-        else:
-            return Response({'status': False})
+            # If started flag is false, fetch latest ended datetime
+            if not result_started or not result_started[0]:
+                query_ended = """
+                    SELECT datetime
+                    FROM cronstatus
+                    WHERE description = 'Cron job ended successfully'
+                    ORDER BY datetime DESC
+                    LIMIT 1;
+                """
+                cursor.execute(query_ended)
+                last_end = cursor.fetchone()
+
+                return Response({
+                    'status': False,
+                    'datetime': last_end[0] if last_end else None
+                })
+
+        # If flag is true, return True
+        return Response({'status': True})
 
 
