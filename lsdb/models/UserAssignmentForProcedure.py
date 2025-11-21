c666516309acb6c7e03262e8b8431407dc36093e
Aravindhan <aravindhan@gesain.net>
2025-11-21 11:44:42 +0530

    removed auto datetime from date field

--- lsdb/models/UserAssignmentForProcedure.py
+++ lsdb/models/UserAssignmentForProcedure.py
@@ -5,7 +5,7 @@ from django.db import models
 class UserAssignmentForProcedure(models.Model):
     user=models.ForeignKey('auth.User', on_delete=models.CASCADE, blank=True, null=True)
     procedure_result=models.ForeignKey('ProcedureResult', on_delete=models.CASCADE, blank=False, null=False)
-    assigned_on=models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    assigned_on=models.DateTimeField(null=True, blank=True)
     assigned_by=models.ForeignKey('auth.User', on_delete=models.CASCADE,related_name='assigned_by', blank=True, null=True)
     due_on=models.IntegerField(null=True,blank=True)
 
