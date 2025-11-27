4dc88d9e9a63a4304e53f327509b4269f6a08a91
GSharu10 <gsharu2000@gmail.com>
2025-11-27 15:06:01 +0530

    Updated code for flash test

--- lsdb/views/NewFlashTestDetailsViewSet.py
+++ lsdb/views/NewFlashTestDetailsViewSet.py
@@ -1,12 +1,12 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-from rest_framework.parsers import JSONParser
 from azure.storage.blob import BlobServiceClient
-from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
+from rest_framework.parsers import MultiPartParser, FormParser
 import uuid
-from lsdb.models import NewFlashTestDetails
+from lsdb.models import NewFlashTestDetails,NewFlashTestPoints
 from lsdb.serializers import NewFlashTestDetailsSerializer
 from rest_framework.permissions import IsAuthenticated
+import json
 
 class NewFlashTestDetailsViewSet(viewsets.ModelViewSet):
     queryset = NewFlashTestDetails.objects.all().order_by('-date_time')
@@ -20,7 +20,6 @@ class NewFlashTestDetailsViewSet(viewsets.ModelViewSet):
         date_time = request.data.get("date_time")
         json_file = request.FILES.get("json_file")
         pdf_file = request.FILES.get("pdf_file")
-        sweep_type = request.data.get("sweep_type")
 
         if not all([serial_number, date_time, json_file, pdf_file]):
             return Response(
@@ -29,17 +28,9 @@ class NewFlashTestDetailsViewSet(viewsets.ModelViewSet):
             )
 
         try:
-            # ------------------------------------------------
-            # 1) Extract values from JSON BEFORE Azure upload
-            # ------------------------------------------------
-            self.extract_ivparams_and_save(json_file, serial_number, sweep_type)
-
-            # Reset pointer after reading
+            self.extract_ivparams_and_save(json_file, serial_number)
             json_file.seek(0)
 
-            # ------------------------------------------------
-            # 2) Azure Upload
-            # ------------------------------------------------
             azure_connection_string = 'DefaultEndpointsProtocol=https;AccountName=haveblueazdev;AccountKey=eP954sCH3j2+dbjzXxcAEj6n7vmImhsFvls+7ZU7F4THbQfNC0dULssGdbXdilTpMgaakIvEJv+QxCmz/G4Y+g==;EndpointSuffix=core.windows.net'
             azure_container = 'flashfiles'
             blob_service_client = BlobServiceClient.from_connection_string(azure_connection_string)
@@ -56,9 +47,6 @@ class NewFlashTestDetailsViewSet(viewsets.ModelViewSet):
             pdf_blob_client.upload_blob(pdf_file, overwrite=True)
             pdf_blob_url = pdf_blob_client.url
 
-            # ------------------------------------------------
-            # 3) Save main NewFlashTestDetails record
-            # ------------------------------------------------
             instance = NewFlashTestDetails.objects.create(
                 serial_number=serial_number,
                 date_time=date_time,
@@ -67,26 +55,20 @@ class NewFlashTestDetailsViewSet(viewsets.ModelViewSet):
                 pdf_file=pdf_filename,
                 pdf_file_path=pdf_blob_url
             )
-
             serializer = self.get_serializer(instance)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
 
-    def extract_ivparams_and_save(self, json_file, serial_number, sweep_type):
-        import json
-        from lsdb.models import NewFlashTestPoints
-
+    def extract_ivparams_and_save(self, json_file, serial_number):
         try:
             parsed_json = json.load(json_file)
             iv = parsed_json.get("IVParams", {})
-
             v_oc_raw = iv.get("v_oc_raw", None)
             v_oc_corr = iv.get("v_oc_corr", None)
             kappa = iv.get("kappa", None)
-
+            sweep_type = iv.get("sweep_type", None)
             point = NewFlashTestPoints.objects.create(
                 serial_number=serial_number,
                 v_oc_raw=v_oc_raw,
@@ -94,9 +76,7 @@ class NewFlashTestDetailsViewSet(viewsets.ModelViewSet):
                 kappa=kappa,
                 sweep_type=sweep_type
             )
-
             return point
-
         except Exception as e:
             print("IVParams Parsing Error:", str(e))
             return None
