8276249075358c021675fbe79a34ab8179d8e853
rohith-913 <rohith@gesain.net>
2026-03-04 13:10:31 +0530

    UMH

--- lsdb/models/UnitMigrationHistory.py
+++ lsdb/models/UnitMigrationHistory.py
@@ -2,11 +2,11 @@ from django.db import models
 from datetime import datetime
 
 class UnitMigrationHistory(models.Model):
-    initial_serial_number = models.CharField(max_length=128, blank=False, null=False)
-    initial_project = models.ForeignKey('Project', on_delete=models.CASCADE, blank=False, null=False)
-    initial_workorder = models.ForeignKey('WorkOrder', on_delete=models.CASCADE, blank=False, null=False)
-    migrated_serial_number = models.CharField(max_length=128, blank=False, null=False)
-    migrated_project = models.ForeignKey('Project', on_delete=models.CASCADE, blank=False, null=False)
-    migrated_workorder = models.ForeignKey('WorkOrder', on_delete=models.CASCADE, blank=False, null=False)
+    initial_serial_number = models.CharField(max_length=128, blank=True, null=True)
+    initial_project = models.ForeignKey('Project', on_delete=models.CASCADE, blank=True, null=True,related_name='initial_project')
+    initial_workorder = models.ForeignKey('WorkOrder', on_delete=models.CASCADE, blank=True, null=True,related_name='initial_workorder')
+    migrated_serial_number = models.CharField(max_length=128, blank=True, null=True)
+    migrated_project = models.ForeignKey('Project', on_delete=models.CASCADE, blank=True, null=True,related_name='migrated_project')
+    migrated_workorder = models.ForeignKey('WorkOrder', on_delete=models.CASCADE, blank=True, null=True,related_name='migrated_workorder')
     migration_date = models.DateTimeField(default=datetime.now)
-    migrated_by = models.ForeignKey('User', on_delete=models.CASCADE, blank=False, null=False)
+    migrated_by = models.ForeignKey('auth.User', on_delete=models.CASCADE, blank=True, null=True,related_name='migration_user')
