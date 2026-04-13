f5f4300dc46b2d52fa34fe16b5bb00d368aba346
Aravindhan <aravindhan@gesain.net>
2026-04-13 17:15:51 +0530

    new fields added in feedback form response

--- lsdb/serializers/FeedBackIOSSerializer.py
+++ lsdb/serializers/FeedBackIOSSerializer.py
@@ -2,12 +2,13 @@ from rest_framework import serializers
 from lsdb.models import FeedBackIOS
 
 class FeedBackIOSSerializer(serializers.ModelSerializer):
-    files = serializers.SerializerMethodField()  
+    files = serializers.SerializerMethodField() 
+    username = serializers.SerializerMethodField() 
     # fb_files = serializers.ListField(child=serializers.FileField(),write_only=True,required=False)
     
     class Meta:
         model = FeedBackIOS
-        fields = '__all__'
+        fields = ['id','files','username','comments','fb_type','priority','created_at','user']
         read_only_fields = ['user', 'created_at']
 
     def get_files(self, obj):
@@ -15,9 +16,16 @@ class FeedBackIOSSerializer(serializers.ModelSerializer):
      
         for fb_files in obj.feedback_files.all():
             azure_id = fb_files.azurefile.id
+            filename = fb_files.azurefile.name
             data.append({
                 "file_id": azure_id,
-                "download_url": f"https://lsdbhaveblueuat.azurewebsites.net/api/1.0/azure_files/{azure_id}/download"
+                "download_url": f"https://lsdbhaveblueuat.azurewebsites.net/api/1.0/azure_files/{azure_id}/download",
+                "filename":filename
             })
 
         return data
+    
+    def get_username(self,obj):
+        return obj.user.username
+
+    
